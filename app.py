a1d4057715525e937a9e46b8f702a5d555a23301
Goi <goisanchezvinicius@gmail.com>
2024-01-16 12:20:31 -0300

    contato estilizado email py configurado

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
-from decouple import config
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.secret_key = "goisanchez"
@@ -10,13 +13,49 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": email,
-    "MAIL_PASSWORD": mail_senha
+    "MAIL_USERNAME": os.getenv("MAIL_USERNAME"),
+    "MAIL_PASSWORD": os.getenv("MAIL_PASSWORD")
 }
+
+app.config.update(mail_settings)
+
+mail = Mail(app)
+
+class Contato:
+    def __init__(self, nome, email, mensagem):
+        self.nome = nome
+        self.email = email
+        self.mensagem = mensagem
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        formContato = Contato(
+            request.form["name"],
+            request.form["email"],
+            request.form["message"]
+        )
+
+        msg = Message(
+            subject= f"{formContato.nome} te enviou um email pelo portfólio",
+            sender= app.config.get("MAIL_USERNAME"),
+            recipients= ['goisanchezvinicius@gmail.com', app.config.get("MAIL_USERNAME")],
+            body = f'''
+
+            {formContato.nome} usando o email {formContato.email}, te enviou algo às <b>{datetime.now().strftime("%H:%M")}</b> no dia <b>{datetime.now().strftime("%d/%m/%Y")}</b>
+            Mensagem:
+
+            {formContato.message}
+
+            '''
+        )
+        mail.send(msg)
+        flash('Email enviado com sucesso!')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
